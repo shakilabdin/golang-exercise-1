2648d0eb3b72314fc5942c4326b6494fdc32b9f4
Shakil Abdin <shakil.abdin@gmail.com>
2020-10-01 20:39:51 -0400

    start

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+
+func main()  {
+	exercise_one()
+	exercise_two()
+}
+
+//exercise 1
+func exercise_one()  {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Println(x, y, z)
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
+
+//exercise 2
+func exercise_two() {
+	var x int
+	var y string
+	var z bool
+
+	fmt.Println(x, y, z)
+}
